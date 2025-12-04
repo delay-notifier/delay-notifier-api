7e319c8ca6062115bb9f21ddbf7fa42a9191bdca
Ablazecornet <yamakou280@gmail.com>
2025-12-04 18:26:21 +0900

    エラー修正 (#8)

--- app/routers/lines.py
+++ app/routers/lines.py
@@ -1,18 +1,11 @@
-from fastapi import APIRouter,  Depends
-from app.schemas import line_schema
+from fastapi import APIRouter
 
 router = APIRouter()
 
 #ある鉄道会社の路線一覧を取得する
-@router.get("/lines", response_model=list[line_schema.Line])
+@router.get("/lines")
 async def get_lines(operator_id: int):
-    return [
-        line_schema.Line = (
-            id=1,
-            name="山手線",
-            operator_id=operator_id
-        )
-    ]
+    pass
 
 #特定の路線の情報を取得する
 @router.get("/v1/lines/{line_id}")
