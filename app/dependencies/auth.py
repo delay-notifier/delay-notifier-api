49e2a81421a83cb70243f48352d9691afc3e3189
Ablazecornet <yamakou280@gmail.com>
2025-12-15 21:29:05 +0900

    add-get_user_by_email

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,8 +1,18 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from app.schemas.user import User
+from typing import Optional
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/login")
+
+async def get_user_by_email(email: str) -> Optional[User]:
+    if email == "test@example.com":
+        return User(
+            id=1,
+            email="test@example.com",
+            password="password123"
+        )
+    return None
 
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     if token != "fake_token":
