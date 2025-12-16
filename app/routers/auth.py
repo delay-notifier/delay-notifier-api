95132b1f01138c6ed2fc183408ccaa3150a671d8
Ablazecornet <yamakou280@gmail.com>
2025-12-16 13:00:26 +0900

    POST-/v1/auth/login (#15)
    * add-POST-/v1/auth/login
    
    * create_access_token関数を修正

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from app.schemas.user import UserCreate, UserResponse, User
 from app.schemas.auth import LoginResponse
-from app.dependencies.auth import get_current_user
+from app.schemas.user import User
+from datetime import timedelta
+from app.dependencies.auth import (
+    get_current_user,
+    get_user_by_email,
+    verify_password,
+    create_access_token,
+)
 
 router = APIRouter()
 
@@ -15,13 +22,28 @@ async def signup(user_body: UserCreate):
 
 @router.post("/login", response_model=LoginResponse)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user: User | None = await get_user_by_email(form_data.username)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+        )
+    if not await verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+        )
+    access_token = create_access_token(
+        data={"sub": str(user.id)},
+        expires_delta=timedelta(minutes=30),
+    )
     return LoginResponse(
-        access_token="fake_token",
+        access_token=access_token,
         token_type="bearer"
     )
 
 @router.post("/logout")
-async def logout():
+async def logout(token: str = Depends(OAuth2PasswordRequestForm)):
     return {"message": "Logged out (dummy)"}
 
 @router.get("/me", response_model=UserResponse)
