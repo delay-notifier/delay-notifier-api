1149e88c39bd12444eb8a7d34e1c881ff4b7ba8c
Yamamoto Kota <yamakou280@gmail.com>
2026-01-04 15:09:52 +0900

    全鉄道会社の一覧取得 (#19)
    * 全鉄道会社の一覧取得
    
    * コメントをもとに修正済み

--- app/routers/lines.py
+++ app/routers/lines.py
@@ -3,12 +3,31 @@ import app.schemas.lines as lines_schema
 
 router = APIRouter()
 
+#全鉄道会社の一覧を取得する
+@router.get("/operators", response_model=lines_schema.OperatorsResponse)
+async def list_operators():
+    return lines_schema.OperatorsResponse(
+        operators=[
+            lines_schema.OperatorBase(
+                    operator_id=1,
+                    operator_name="JR東日本"
+            ),
+            lines_schema.OperatorBase(
+                    operator_id=2,
+                    operator_name="小田急電鉄"
+            ),
+            lines_schema.OperatorBase(
+                    operator_id=3,
+                    operator_name="西武鉄道"
+            )
+        ]
+    )
+
 #ある鉄道会社の路線一覧を取得する
-@router.get("/{operator_id}", response_model=list[lines_schema.OperatorResponse])
-async def get_lines(operator_id: int):
-    return [
-        lines_schema.OperatorResponse(
-            operator_id=1,
+@router.get("/operators/{operator_id}", response_model=lines_schema.OperatorResponse)
+async def list_lines(operator_id: int):
+    return lines_schema.OperatorResponse(
+            operator_id=operator_id,
             operator_name="JR東日本",
             lines=[
                 lines_schema.LineResponse(
@@ -21,12 +40,8 @@ async def get_lines(operator_id: int):
                 )
             ]
         )
-    ]
-    
 
 #特定の路線の情報を取得する
 @router.get("/{line_id}")
 async def get_lines(line_id: int):
     pass
-
-#test
--- app/schemas/lines.py
+++ app/schemas/lines.py
@@ -1,19 +1,26 @@
 from pydantic import BaseModel, Field
 from typing import List
 
-class LineBase(BaseModel):
-    operator_id: int = Field(None, description="鉄道会社ID")
-    operator_name: str = Field(None, description="鉄道会社名")
+class OperatorBase(BaseModel):
+    operator_id: int = Field(..., description="鉄道会社ID")
+    operator_name: str = Field(..., description="鉄道会社名")
 
-class LineResponse(BaseModel):
-    line_id: int = Field(None, description="路線ID")
-    line_name: str = Field(None, description="路線名")
+class LineBase(BaseModel):
+    line_id: int = Field(..., description="路線ID")
+    line_name: str = Field(..., description="路線名")
+
+class LineResponse(LineBase):
+    class Config:
+        orm_mode = True
+
+class OperatorResponse(OperatorBase):
+    lines: List[LineResponse] = Field([], description="路線一覧")
 
     class Config:
         orm_mode = True
 
-class OperatorResponse(LineBase):
-    lines: List[LineResponse] = Field([], description="路線一覧")
+class OperatorsResponse(BaseModel):
+    operators: List[OperatorBase] = Field([], description="全鉄道会社")
 
     class Config:
         orm_mode = True
